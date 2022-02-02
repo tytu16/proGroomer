a68152d42bc0b88589cda7a26f0a0851f00ef1ed
Tyler T <ttuschhoff11@gmail.com>
2022-02-02 17:48:10 -0600

    Profile now extends PersonInfo

--- src/models/Profile.tsx
+++ src/models/Profile.tsx
@@ -1,19 +1,16 @@
 import {AccountInfo} from "./AccountInfo"
 import { PersonInfo } from "./PersonInfo";
 
-export class Profile {
-    private _id: string;
+export class Profile extends PersonInfo {
     private _isPaid: boolean;
-    private _personalInfo: PersonInfo;
     private _accounts: Array<AccountInfo>
 
     constructor(data: any){
         if(!data){
             data = {};
         }
-        this._id = data.id ? data.id : '0';
+        super(data);
         this._isPaid = data.isPaid ? data.isPaid : true;
-        this._personalInfo = data.personalInfo ? data.personalInfo : new PersonInfo({});
         if(data.accounts){
             let tempAccounts = Array<AccountInfo>();
             for(let account of data.accounts){
@@ -25,25 +22,11 @@ export class Profile {
         }
     }
 
-    get id(): string{
-        return this._id;
-    }
-    set id(id: string){
-        this._id = id;
-    }
-
     get isPaid(): boolean{
         return this._isPaid;
     }
     set isPaid(isPaid: boolean){
         this._isPaid = isPaid;
-    }
-
-    get personalInfo(): PersonInfo {
-        return this._personalInfo;
-    }
-    set personalInfo(personalInfo: PersonInfo){
-        this._personalInfo = personalInfo;
     }
     
     get accounts(): Array<AccountInfo>{
@@ -52,6 +35,7 @@ export class Profile {
     set accounts(accounts: Array<AccountInfo>){
         this._accounts = accounts;
     }
+ 
     addAccount(newAccount: AccountInfo){
         this._accounts = [...this._accounts, newAccount];
     }
